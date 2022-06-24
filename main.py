258afdeb9817fc34bd21506bc598c82a57b41fa4
SidD <siddhuiiti@hotmail.com>
2022-06-24 10:12:14 +0530

    Good Initial Commit

---
+++ main.py
@@ -0,0 +1,117 @@
+from urllib import response
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+from fastapi import BackgroundTasks
+import utils
+import time, asyncio
+from fastapi.encoders import jsonable_encoder
+
+class Course(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: int
+    author: Optional[str] = None
+
+class TextFileModel(BaseModel):
+    language: str
+    id: int
+    text: str 
+
+class Model(BaseModel):
+    text_to_analyze: list[TextFileModel]
+
+app = FastAPI()
+
+course_items = [{"course_name":"python"}, {"course_name":"NodeJS"}, {"course_name":"PMP"}, {"course_name":"Excel"}]
+
+@app.post("/courses/")
+def create_course(course: Course):
+    course_items.append(course)
+    return course
+
+@app.get("/")
+def root():
+    return {"message": "Hello world"}
+
+
+@app.get("/courses/{course_id}")
+def read_course(course_id:int, q: Optional[str] = None ):
+    if q is not None:
+        return {"course_name": course_items[course_id], "Quest": q}
+    return {"course_name": course_items[course_id]}
+
+
+#setted default value for start and end
+@app.get("/courses/")
+def read_courses(start: int = 0, end: int = 1):
+    return course_items[start: start+end]
+
+
+# Define background tasks to be run after returning a response( acknowledgement or confirmation )
+def write_notification(email:str, message=""):
+    with open("log.txt",mode ="w") as email_file:
+        content = f"notification for {email}: {message}"
+        email_file.write(content)
+
+@app.post("/send-notification/{email}")
+def send_notification(email:str, background_tasks: BackgroundTasks):
+    background_tasks.add_task(write_notification,email,message="Some NOtificatoin")
+    return {"message":"NOtification sent in the background"}
+
+
+# Define concurrent tasks
+@app.get("/concur")
+async def home():
+    tasks = []
+    start = time.time()
+    for i in range(2):
+        tasks.append(asyncio.create_task(func1()))
+        tasks.append(asyncio.create_task(func2()))
+
+    # We gather all the tasks and wait for them to be completed
+    response = await asyncio.gather(*tasks)
+    end = time.time()
+
+    return {"response": response, "time_taken": (end - start)}
+
+async def func1():
+
+    #Await says it is safe to switch to another coroutine in the event loop
+    await asyncio.sleep(1)
+    return "Func1 completed"
+
+async def func2():
+    await asyncio.sleep(2)
+    return "Func2 completed"
+
+@app.post("/analyze/")
+async def analyze_text(text: Model):
+    response = { "sentiment": [], "keyphrases":[]}
+
+    list_docs = json_compatible_item_data = jsonable_encoder(text.text_to_analyze)
+    no_of_text = len(text.text_to_analyze)
+
+    
+    document = {"documents": list_docs }
+    
+    tasks = []
+    tasks.append(asyncio.create_task(utils.get_sentiment(document=document)))
+    tasks.append(asyncio.create_task(utils.get_key_phrases(document=document)))
+
+    # We gather all the tasks and wait for them to be completed
+    doc_analysis = await asyncio.gather(*tasks)
+    doc_senti = doc_analysis[0]
+    doc_keyphrases = doc_analysis[1]
+
+    #doc_senti = utils.get_sentiment(document=document)
+    #doc_keyphrases = utils.get_key_phrases(document=document)
+    
+    for i in range(no_of_text):
+        try:
+            response["sentiment"].append(doc_senti[i]['sentiment'])
+            response["keyphrases"].append(doc_keyphrases[i]['keyPhrases'])
+        except:
+            print( "No response {1}",i)
+
+    return response
---
+++ utils.py
@@ -0,0 +1,38 @@
+from ast import Try
+import requests as req
+import os
+
+api_key = os.environ['AZURE_TEXTANA_KEY']
+
+async def call_text_analytics_api(document,  endpoint, headers=None):
+    if headers is None:
+        headers = { "Ocp-Apim-Subscription-Key": api_key,
+                    "Content-Type": "application/json",
+                    "Accept": "application/json" }
+    response = req.post("https://educativesidtext.cognitiveservices.azure.com//text/analytics/v3.0/" + endpoint, headers=headers, json=document)
+    return response.json()
+
+async def get_key_phrases(document):
+    endpoint = 'keyPhrases'
+    try:
+        api_response = await call_text_analytics_api( document, endpoint=endpoint)
+        keyPhrases = api_response["documents"]
+        for i in range(len(keyPhrases)):
+            document_level_keyphrases = keyPhrases[i]["keyPhrases"]
+            print("Document {}: KyePhrases: {}".format(i+1, document_level_keyphrases))
+        return keyPhrases
+    except KeyError:
+        print(document)
+        print(api_response)
+
+async def get_sentiment(document):
+    try:
+        endpoint = 'sentiment'
+        api_response = await call_text_analytics_api( document, endpoint=endpoint)
+        sentiments = api_response["documents"]
+        for i in range(len(sentiments)):
+            print("Document {0}: Sentiment: {1} ".format(i + 1, sentiments[i]["sentiment"]))
+        return sentiments
+    except KeyError:
+        print(document)
+        print(api_response)
